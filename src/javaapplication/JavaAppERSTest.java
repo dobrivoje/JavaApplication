ddb7292b04ee370b13c718a1b127d44b8d1f5461
dobri <dobri@rcdell.akk_domain.com>
2014-04-17 16:20:12 +0200

    Testiranja FX TimeLine

--- src/javaapplication/JavaAppERSTest.java
+++ src/javaapplication/JavaAppERSTest.java
@@ -7,11 +7,9 @@ package javaapplication;
 
 import ERS.TimeLine.Functionalities.Adapters.FirmaAdapter;
 import ERS.TimeLine.Functionalities.ITimeLineCategory;
-import ERS.TimeLine.Functionalities.Adapters.RadnikAdapter;
 import ERS.TimeLine.Functionalities.ITimeLineDuration;
 import ERS.TimeLine.Functionalities.ITimeLineObservableUnit;
 import ERS.queries.ERSQuery;
-import ent.Firma;
 import java.text.ParseException;
 import java.util.Map;
 
@@ -22,32 +20,21 @@ import java.util.Map;
 public class JavaAppERSTest {
 
     public static void main(String[] args) throws ParseException {
-        String Datum = "2009-4-9";
-        ITimeLineCategory r2 = new RadnikAdapter(66l);
-        Firma Firma = ERSQuery.PODRAZUMEVANA_FIRMA;
-        ITimeLineObservableUnit OU = new FirmaAdapter(Firma, Datum);
+        String Datum = "2014-4-19";
+        ITimeLineObservableUnit OU = new FirmaAdapter(ERSQuery.PODRAZUMEVANA_FIRMA, Datum);
 
-        System.out.println("ITimeLineCategory Radnik - " + r2.getCategory());
-        System.out.println("Podrazumevana Firma : " + Firma.toString());
-
-        /*
-         ITimeLineObservableUnit OU = new FirmaAdapter(ERSQuery.PODRAZUMEVANA_FIRMA, Datum);
-         for (ITimeLineCategory cat : OU.getCategories()) {
-         System.out.println("Kategorija - " + cat.getCategory() + ", Radnik - " + ((RadnikAdapter)cat).getRadnik().getSifraINFSISTEM());
-         }
-         */
         //Test
-        for (Map.Entry<String, Map<Integer, ITimeLineDuration>> entry : ERSQuery.AllCategoresEvents(OU, Datum).entrySet()) {
-            String CAT = entry.getKey();
-            Map<Integer, ITimeLineDuration> DOGADJAJI = entry.getValue();
+        for (Map.Entry<ITimeLineCategory, Map<Integer, ITimeLineDuration>> kat : ERSQuery.AllCategoresEvents(OU, Datum).entrySet()) {
+            String CAT = kat.getKey().getCategory();
+            Map<Integer, ITimeLineDuration> DOGADJAJI = kat.getValue();
 
             System.out.println(CAT);
 
-            for (Map.Entry<Integer, ITimeLineDuration> entry1 : DOGADJAJI.entrySet()) {
-                Integer I = entry1.getKey();
-                ITimeLineDuration D = entry1.getValue();
+            for (Map.Entry<Integer, ITimeLineDuration> e1 : DOGADJAJI.entrySet()) {
+                Integer I = e1.getKey();
+                ITimeLineDuration D = e1.getValue();
 
-                System.out.println(I + " -> " + D);
+                System.out.println("    " + I + " -> " + D);
             }
         }
     }
